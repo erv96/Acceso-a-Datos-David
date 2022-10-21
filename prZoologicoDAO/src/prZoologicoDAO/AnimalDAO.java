3a96c6ac11450f998d1c0a55f75228b30b5a8d96
David <toledo18696@gmail.com>
2022-10-21 13:35:23 +0200

    actualizacion

--- prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
+++ prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
@@ -2,6 +2,7 @@ package prZoologicoDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,8 +10,55 @@ public abstract class AnimalDAO {
 
 	private static Connection connection;
 	
+	//Buscar animal por id 
+	
+	public static Animal findById(int id) {
+		connection = openConnection();
+		Animal animal = null;
+		
+		String query = "select * from animales where id=?";
+		PreparedStatement ps;
+		try {
+			ps = connection.prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				animal = new Animal(
+						rs.getInt("id"),
+						rs.getString("nombre"),
+						rs.getDouble("peso_aproximado"),
+						rs.getString("habitat"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		closeConnection();
+		return animal;
+	}
+	
+	//Borrar un animal dado su nombre 
+	public static void deleteAnimalByNombre(String nombre) {
+		connection = openConnection();
+		String query = "delete from animales where nombre = ?";
+		
+		try {
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, nombre);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		closeConnection();
+		
+	}
+	
 	//Borrar todos los animales 
-	public static void deleteAnimal() {
+	public static void deleteAllAnimales() {
 		connection = openConnection();
 		
 		String query = "delete from animales";
@@ -18,6 +66,11 @@ public abstract class AnimalDAO {
 		try {
 			Statement statement = connection.createStatement();
 			statement.executeUpdate(query);
+			
+			//Misma operacion con preparedStatement
+			
+			/*PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.executeUpdate();*/
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- prZoologicoDAO/src/prZoologicoDAO/Main.java
+++ prZoologicoDAO/src/prZoologicoDAO/Main.java
@@ -5,8 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Animal a = new Animal("Ardilla", 0.1, "Bosque");
-		AnimalDAO.insertAnimal(a);
-		//AnimalDAO.deleteAnimal();
+		//AnimalDAO.insertAnimal(a);
+		//AnimalDAO.deleteAllAnimales();
+		//AnimalDAO.deleteAnimalByNombre("Ardilla");
+		Animal b = AnimalDAO.findById(6);
+		System.out.println(b);
+		
+		
+		
 
 	}
 
